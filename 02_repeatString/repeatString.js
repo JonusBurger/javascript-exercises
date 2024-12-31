c61836a223ee873c760c0063b666bc398fbd78dc
Jberg <jberg@Xubuntu.myguest.virtualbox.org>
2024-12-31 11:13:16 +0100

    solve first 2 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, timesRepeat) {
+    let outputString ="";
+    if (timesRepeat < 0){
+        return "ERROR"
+    }
+    for (let i = 0; i < timesRepeat; i++){
+        outputString += string;
+    }
+    return (outputString);
 };
 
 // Do not edit below this line
