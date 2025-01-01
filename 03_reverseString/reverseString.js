ddea016781b9b234a64daaeeec2f998e376c963a
Jberg <jberg@Xubuntu.myguest.virtualbox.org>
2025-01-01 10:07:18 +0100

    solved first exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
 const reverseString = function() {
+    /* Turn into arry */
+    
+    /*Reverse array*/
 
+    /*Joinas Return statement*/
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, args) {
+    /* Copy Array */
+    let outputArray = array;
+    /* Loop through optional input */
+    for (let i = 0; i < args.length; i++){
+        /* Remove entry based on argument (pop?) -> Ensure to Pop if equal -> either Loop (cheap version?) or use Javascript function -> forEach?*/
+        outputArray = outputArray.filter(filterArray(item, args[i]));
+    }   
+    return outputArray
 };
+function filterArray(item, criteria){
+    if (item != criteria){
+        return item;
+    }
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
