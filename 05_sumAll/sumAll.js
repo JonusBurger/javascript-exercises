a341f50c309a102a634159eb61efde07d7763b62
JonusBurger <j.bergner@live.de>
2025-01-02 11:53:29 +0100

    solve rest of exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(input1, input2) {
+    if (!Number.isInteger(input1)|| input1 < 0 || input2 < 0 || !Number.isInteger(input2)){
+        return "ERROR"
+    }
+    let start;
+    let stop;
+    if (input1 > input2){
+        start = input2;
+        stop = input1;
+    } else{
+        start = input1;
+        stop = input2;
+    }
 
+    let result = 0;
+    for (let i = start; i <= stop; i++){
+        result += i;
+    }
+    return result
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempratur) {
+  let result = (tempratur-32)*(5/9);
+  let resultRounded = Math.round(result*10) / 10;
+  return resultRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempratur) {
+  let result = tempratur*(9/5)+32;
+  let resultRounded = Math.round(result*10) / 10;
+  return resultRounded;
 };
 
 // Do not edit below this line
